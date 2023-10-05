1e1f0e7e6afb15434f26db0805936d5b39dc30b0
Lyz <61083437+00redi@users.noreply.github.com>
2023-10-05 07:14:07 +0200

    Add files via upload

---
+++ fog.js
@@ -0,0 +1,14 @@
+VANTA.FOG({
+    el: "body",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    highlightColor: 0x0,
+    midtoneColor: 0xffffff,
+    lowlightColor: 0x8600ff,
+    blurFactor: 0.7,
+    speed: 1.5,
+    zoom: 1,
+  })
